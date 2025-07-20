cf58cf13d28873183f05b5f9038e443ef6d1d142
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2025-07-20 17:54:20 +0530

    barebones ui

--- build/ui.js
+++ build/ui.js
@@ -1,6 +1,8 @@
 import { setupPhysics, Time } from "./physics.js";
 import { crash, getElement } from "./util-funcs.js";
 const canvas = getElement("canvas", HTMLCanvasElement);
+const pause = getElement("pause", HTMLSpanElement);
+const nextFrame = getElement("next-frame", HTMLSpanElement);
 function setCanvasSize() {
     if (canvas.width != innerWidth)
         canvas.width = innerWidth;
@@ -9,10 +11,39 @@ function setCanvasSize() {
 }
 const updatePhysics = setupPhysics();
 const ctx = canvas.getContext('2d') ?? crash('canvas not supported');
+let paused = Boolean(localStorage.getItem('physics-paused'));
+pause.addEventListener("click", () => {
+    paused = !paused;
+    Time.reset();
+    pause.innerText = paused ? '▶️' : '⏸️';
+    pause.title = paused ? 'Resume' : 'Pause';
+    nextFrame.classList[paused ? 'remove' : 'add']('disabled');
+});
+nextFrame.addEventListener("click", () => {
+    if (!paused)
+        return;
+    ctx.fillStyle = `#cceeFF`;
+    ctx.fillRect(0, 0, innerWidth, innerHeight);
+    Time.reset();
+    updatePhysics(ctx);
+});
+window.addEventListener("keydown", e => {
+    if (e.key === "ArrowRight") {
+        nextFrame.click();
+        e.preventDefault();
+    }
+    if (e.key === " ") {
+        pause.click();
+        e.preventDefault();
+    }
+});
 (function loop() {
     setCanvasSize();
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
     Time.update();
-    updatePhysics(ctx);
+    if (!paused) {
+        ctx.fillStyle = `#cceeFF`;
+        ctx.fillRect(0, 0, innerWidth, innerHeight);
+        updatePhysics(ctx);
+    }
     requestAnimationFrame(loop);
 })();
